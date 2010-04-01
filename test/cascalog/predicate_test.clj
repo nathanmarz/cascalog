c49e5d6471f941850e4ac63042f7560b2433d7f0
Nathan Marz <nathan.marz@gmail.com>
2010-03-31 17:59:20 -0700

    tests for nil filtering, added stubs for more tests

--- test/cascalog/predicate_test.clj
+++ test/cascalog/predicate_test.clj
@@ -19,9 +19,6 @@
 (w/defmapop timesplusone "blahfield" [a b]
   (inc (* a b)))
 
-(w/defmapop addplusone "blah" [& all]
-  [(inc (apply + all)) (first all)])
-
 (deftest test-map-pred
   (let [pred (build-predicate timesplusone (var timesplusone) "?f1" "?f2" :> "?q")
         source-data {:fields ["?a" "?b" "?f1" "?f2" "?c"] :tuples [[1 2 1 1 10]
@@ -33,6 +30,9 @@
     (is (= ["?q"] (:outfields pred)))
     (test-assembly source-data sink-data (:assembly pred))
     ))
+
+(w/defmapop addplusone ["blah" "blah2"] [& all]
+  [(inc (apply + all)) (first all)])
 
 (deftest test-map-constants
   (let [pred (build-predicate addplusone (var addplusone) "?f1" "?f2" 3 4 "?f3" :> "?s" 6)
@@ -47,3 +47,24 @@
     (is (= 5 (count (:outfields pred))))
     (test-assembly source-data sink-data (:assembly pred))
     ))
+
+(w/defmapop nilop ["f1" "f2"] [a]
+  (if (not= a 1) [a nil] [nil a]))
+
+(deftest test-nil-filtering
+  (let [pred (build-predicate nilop (var nilop) "?i" :> "?o1" "!o2")
+        source-data {:fields ["?i"] :tuples [[1]
+                                             [2]
+                                             [3]
+                                             [1]]}
+        sink-data   {:fields ["?o1" "!o2"] :tuples [[2 nil] [3 nil]]} ]
+     (test-assembly source-data sink-data (:assembly pred))
+     ))
+
+(deftest test-filter-pred)
+
+(deftest test-vanilla-filter)
+
+(deftest test-vanilla-func)
+
+(deftest test-generator)
