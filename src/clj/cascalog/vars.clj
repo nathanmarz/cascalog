816b2668530a9f490518bbe21329601d79f2a08c
Nathan Marz <nathan.marz@gmail.com>
2010-04-08 15:30:34 -0700

    support for _ variables

--- src/clj/cascalog/vars.clj
+++ src/clj/cascalog/vars.clj
@@ -1,13 +1,21 @@
 (ns cascalog.vars)
 
+;; TODO: better to use UUIDs to avoid name collisions with client code?
+;; Are the size of fields an issue in the actual flow execution perf-wise?
+(let [i (atom 0)]
+  (defn gen-unique-suffix [] (str "__gen" (swap! i inc))))
+
+(defn gen-nullable-var [] (str "!" (gen-unique-suffix)))
+
 (defn- extract-varname [v]
-  (if (symbol? v) (name v) v))
+  (let [actname (if (symbol? v) (name v) v)]
+    (if (= "_" actname) (gen-nullable-var) actname)))
 
 (defn cascalog-var? [obj]
     (if (or (symbol? obj) (string? obj))
       (let [obj (extract-varname obj)]
-        ((complement nil?) (some #(.startsWith obj %) ["?" "!" "!!"])))
-        false ))
+          ((complement nil?) (some #(.startsWith obj %) ["?" "!" "!!"])))
+      false ))
 
 (defn non-nullable-var? [sym-or-str]
   (.startsWith (extract-varname sym-or-str) "?"))
@@ -21,13 +29,6 @@
 
 (defn vars2str [vars]
   (vec (map #(if (cascalog-var? %) (extract-varname %) %) vars)))
-
-;; TODO: better to use UUIDs to avoid name collisions with client code?
-;; Are the size of fields an issue in the actual flow execution perf-wise?
-(let [i (atom 0)]
-  (defn gen-unique-suffix [] (str "__gen" (swap! i inc))))
-
-(defn gen-nullable-var [] (str "!" (gen-unique-suffix)))
 
 (defn uniquify-vars
   "Uniques the cascalog vars, same vars still have the same name"
--- test/cascalog/core_test.clj
+++ test/cascalog/core_test.clj
@@ -7,6 +7,6 @@
 
 (deftest test-basic-query
   (with-tmp-sources [friends {:fields ["p1" "p2"] :tuples [["n" "j"] ["j" "m"] ["m" "a"]]}
-                     ages {:fields ["p" "a"] :tuples [["n" 25] ["m" 30]]}]
-    (?<- (w/lfs-tap (w/text-line ["p1" "p2"]) "/tmp/aaa") [?p1 ?p3] (friends ?p1 ?p2) (friends ?p2 ?p3))
+                     age {:fields ["p" "a"] :tuples [["n" 25] ["m" 30]]}]
+    (?<- (w/lfs-tap (w/text-line ["p1" "p2"]) "/tmp/aaa") [?p1 ?p3] (age ?p1 _) (friends ?p1 ?p2) (friends ?p2 ?p3))
    ))
