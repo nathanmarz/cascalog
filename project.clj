ee11ccf091d0a32c84a926fb2736357b9fcd919d
Nathan Marz <nathan.marz@gmail.com>
2010-04-11 19:36:27 -0700

    dependencies all managed through leiningen now

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject cascalog "1.0.0-SNAPSHOT"
   :source-path "src/clj"
-  :library-path "_deps"
   :java-source-path "src/jvm"
   :java-fork "true"
   :javac-debug "true"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
+                 [org.clojure/clojure-contrib "1.1.0"]
+                 [cascading1.1 "1.1.3-SNAPSHOT"]
+                 [org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                 ]
   :dev-dependencies [[lein-javac "0.0.2-SNAPSHOT"]]
   :namespaces [cascalog.workflow
                cascalog.api
