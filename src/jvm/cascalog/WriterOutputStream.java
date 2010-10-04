8e424150e8f13ae768cad5157c27fcde7f8919a9
Nathan Marz <nathan.marz@gmail.com>
2010-10-04 16:50:33 -0700

    helper class for running jobs within emacs repl

---
+++ src/jvm/cascalog/WriterOutputStream.java
@@ -0,0 +1,33 @@
+package cascalog;
+
+import java.io.OutputStream;  
+import java.io.IOException;  
+import java.io.Writer;  
+   
+/**  
+ * This is for helping with running hadoop jobs from within emacs repl.
+ */  
+public class WriterOutputStream extends OutputStream {  
+   
+    private final Writer writer;  
+   
+    public WriterOutputStream(Writer writer) {  
+        this.writer = writer;  
+    }  
+   
+    public void write(int b) throws IOException {  
+        write(new byte[] {(byte) b}, 0, 1);  
+    }  
+   
+    public void write(byte b[], int off, int len) throws IOException {  
+        writer.write(new String(b, off, len));  
+    }  
+   
+    public void flush() throws IOException {  
+        writer.flush();  
+    }  
+   
+    public void close() throws IOException {  
+        writer.close();  
+    }  
+}  
