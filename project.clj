c1bd29769640c1dd9bf8c6dd727d1128d5dbf188
Nathan Marz <nathan.marz@gmail.com>
2010-11-10 17:09:58 -0800

    cascalog 1.4.0, change cascading dependency to conjars

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cascalog "1.4.0-SNAPSHOT"
+(defproject cascalog/cascalog "1.4.0"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :java-fork "true"
@@ -6,9 +6,10 @@
   :hooks [leiningen.hooks.javac]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [cascading1.1 "1.1.3-SNAPSHOT"]
+                 [cascading/cascading-core "1.1.3"]
                  [log4j/log4j "1.2.16"]
                  ]
+  :repositories {"conjars" "http://conjars.org/repo/"}
   :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]
                      [org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [swank-clojure "1.2.1"]
