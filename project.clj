bff6625f9c39c6338e238d61fbf9ae61dc562f30
Nathan Marz <nathan.marz@gmail.com>
2010-12-07 11:56:09 -0800

    upgrade cascalog build to leiningen 1.4.0

--- project.clj
+++ project.clj
@@ -3,15 +3,14 @@
   :java-source-path "src/jvm"
   :java-fork "true"
   :javac-debug "true"
-  :hooks [leiningen.hooks.javac]
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [cascading/cascading-core "1.2-wip-63" :exclusions [org.codehaus.janino/janino]]
                  [log4j/log4j "1.2.16"]
                  ]
   :repositories {"conjars" "http://conjars.org/repo/"}
-  :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]
+  :dev-dependencies [
                      [org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [swank-clojure "1.2.1"]
                     ]
-  :namespaces :all)
+  :aot :all)
