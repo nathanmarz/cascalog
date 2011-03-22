424d9a779df8884b57597f64df9e104a51aeb4af
Nathan Marz <nathan.marz@gmail.com>
2011-03-21 17:05:14 -0700

    workaround bug in cascading's pom so that users can get cascalog + deps

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :javac-options {:debug "true" :fork "true"}
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [cascading/cascading-core "1.2-wip-63" :exclusions [org.codehaus.janino/janino]]
+                 ;; jgrapht exclusion works around cascading pom bug that causes projects dependent on
+                 ;; cascalog to not be able to find jgrapht
+                 [cascading/cascading-core "1.2-wip-63" :exclusions [org.codehaus.janino/janino thirdparty/jgrapht-jdk1.6]]
+                 [thirdparty/jgrapht-jdk1.6 "0.8.1"]
                  [log4j/log4j "1.2.16"]
                  ]
   :repositories {"conjars" "http://conjars.org/repo/"}
