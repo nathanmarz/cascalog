a1a0f494c5453cf001a65341cdff541af8d78cd2
Sam Ritchie <sritchie09@gmail.com>
2011-07-28 16:50:47 -0400

    Added defmain.

--- src/clj/cascalog/api.clj
+++ src/clj/cascalog/api.clj
@@ -348,3 +348,19 @@ as well."
   [& body]
   `(binding [cascalog.debug/*DEBUG* true]
      ~@body))
+
+;; Class Creation
+
+(defmacro defmain
+  "Defines an AOT-compiled function with the supplied
+  `name`. Containing namespace must be marked for AOT compilation to
+  have any effect."
+  [name & forms]
+  (let [classname (str *ns* "." name)
+        sym (with-meta
+              (symbol (str name "-main"))
+              (meta name))]
+    `(do (gen-class :name ~classname
+                    :main true
+                    :prefix ~(str name "-"))
+         (defn ~sym ~@forms))))
