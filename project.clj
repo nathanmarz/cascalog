863957a8840be0949f6d9b8a68c5c70eb76ecbc2
Sam Ritchie <sritchie09@gmail.com>
2011-09-26 15:40:39 -0700

    added project.clj updates for 1.8.1

--- project.clj
+++ project.clj
@@ -1,12 +1,14 @@
-(defproject cascalog/cascalog "1.8.1-SNAPSHOT"
+(defproject cascalog/cascalog "1.8.1"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :jvm-opts ["-Xmx768m" "-server"]
   :javac-options {:debug "true" :fork "true"}
+  :repositories {"conjars" "http://conjars.org/repo/"}
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 ;; jgrapht exclusion works around cascading pom bug that causes projects dependent on
-                 ;; cascalog to not be able to find jgrapht
+                 ;; jgrapht exclusion works around cascading pom bug
+                 ;; that causes projects dependent on cascalog to not
+                 ;; be able to find jgrapht.
                  [cascading/cascading-core "1.2.4"
                   :exclusions [org.codehaus.janino/janino
                                thirdparty/jgrapht-jdk1.6
@@ -14,7 +16,6 @@
                  [thirdparty/jgrapht-jdk1.6 "0.8.1"]
                  [riffle/riffle "0.1-dev"]
                  [log4j/log4j "1.2.16"]]
-  :repositories {"conjars" "http://conjars.org/repo/"}
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
                      [clojure-source "1.2.0"]
                      [org.apache.hadoop/hadoop-core "0.20.2-dev"]]
