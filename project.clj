6b836e73d94429b5e83cfbbe26f6085d90f97e78
Sam Ritchie <sritchie09@gmail.com>
2011-12-05 11:06:33 -0800

    Bump to a version of cascading.kryo that guarantees ordering of custom serializations.

--- project.clj
+++ project.clj
@@ -13,8 +13,8 @@
                                thirdparty/jgrapht-jdk1.6
                                riffle/riffle]]
                  [thirdparty/jgrapht-jdk1.6 "0.8.1"]
-                 [cascading.kryo "0.1.0"]
-                 [cascalog/carbonite "1.0.1"]
+                 [cascading.kryo "0.1.1"]
+                 [cascalog/carbonite "1.0.2"]
                  [riffle/riffle "0.1-dev"]
                  [log4j/log4j "1.2.16"]
                  [hadoop-util "0.2.4"]]
