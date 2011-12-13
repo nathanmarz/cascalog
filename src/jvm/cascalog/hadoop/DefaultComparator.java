8530bb28c860c48529057b777a4e3ca7ca372dd7
Sam Ritchie <sritchie09@gmail.com>
2011-12-12 16:10:45 -0800

    added DefaultComparator.

---
+++ src/jvm/cascalog/hadoop/DefaultComparator.java
@@ -0,0 +1,37 @@
+package cascalog.hadoop;
+
+import cascading.tuple.hadoop.TupleSerialization;
+import clojure.lang.Util;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+/** User: sritchie Date: 12/12/11 Time: 3:23 PM */
+public class DefaultComparator extends Configured implements Comparator, Serializable {
+
+    TupleSerialization serialization;
+
+    public DefaultComparator() {
+    }
+
+    public DefaultComparator( Configuration conf ) {
+        super( conf );
+    }
+
+    @Override public void setConf( Configuration conf ) {
+        if( conf == null ) return;
+        super.setConf( conf );
+        serialization = new TupleSerialization( conf );
+    }
+
+    private Comparator getComparator(Class klass) {
+        return (serialization == null) ? null : serialization.getComparator(klass);
+    }
+
+    public int compare(Object o1, Object o2) {
+        Comparator comp = getComparator(o1.getClass());
+        return (comp == null) ? Util.compare(o1, o2) : comp.compare(o1, o2);
+    }
+}
