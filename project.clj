268fb2a10014ba22275b2ec4046391daaef3cdbd
Sam Ritchie <sritchie09@gmail.com>
2011-12-13 23:53:24 -0800

    Bumped carbonite version.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                                riffle/riffle]]
                  [thirdparty/jgrapht-jdk1.6 "0.8.1"]
                  [cascading.kryo "0.1.4"]
-                 [cascalog/carbonite "1.0.3"]
+                 [cascalog/carbonite "1.0.4"]
                  [riffle/riffle "0.1-dev"]
                  [log4j/log4j "1.2.16"]
                  [hadoop-util "0.2.4"]]
@@ -27,7 +27,7 @@
                                     riffle/riffle]]
                       [thirdparty/jgrapht-jdk1.6 "0.8.1"]
                       [cascading.kryo "0.1.4"]
-                      [cascalog/carbonite "1.0.3"]
+                      [cascalog/carbonite "1.0.4"]
                       [riffle/riffle "0.1-dev"]
                       [log4j/log4j "1.2.16"]
                       [hadoop-util "0.2.4"]]})
