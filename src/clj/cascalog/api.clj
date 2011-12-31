af235bece8891c42e7c250ea1f989187bb338f23
Sam Ritchie <sritchie09@gmail.com>
2011-12-30 22:27:46 -0800

    Add defalias to defmain, so that the generated functions are callable.

--- src/clj/cascalog/api.clj
+++ src/clj/cascalog/api.clj
@@ -390,4 +390,5 @@ as well."
     `(do (gen-class :name ~classname
                     :main true
                     :prefix ~(str name "-"))
-         (defn ~sym ~@forms))))
+         (defn ~sym ~@forms)
+         (defalias ~name ~sym))))
