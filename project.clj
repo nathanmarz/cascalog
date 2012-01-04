e40cc777db91ab70852d7d15f4f0b8dc3f3ca299
Sam Ritchie <sritchie09@gmail.com>
2012-01-04 10:01:35 -0800

    Proper jackknife version.

--- project.clj
+++ project.clj
@@ -34,4 +34,4 @@
                       [riffle/riffle "0.1-dev"]
                       [log4j/log4j "1.2.16"]
                       [hadoop-util "0.2.6"]
-                      [jackknife "0.1.1"]]})
+                      [jackknife "0.1.2"]]})
