69587fe6e7759073c4dc4730fdf765cdbbd81372
Sam Ritchie <sritchie09@gmail.com>
2012-01-10 20:23:34 -0500

    Share dependencies between multi-deps and deps.

--- project.clj
+++ project.clj
@@ -1,3 +1,20 @@
+(def shared-deps
+  '[[org.clojure/tools.macro "0.1.1"]
+    ;; jgrapht exclusion works around cascading pom bug
+    ;; that causes projects dependent on cascalog to not
+    ;; be able to find jgrapht.
+    [cascading/cascading-core "1.2.4"
+     :exclusions [org.codehaus.janino/janino
+                  thirdparty/jgrapht-jdk1.6
+                  riffle/riffle]]
+    [thirdparty/jgrapht-jdk1.6 "0.8.1"]
+    [cascading.kryo "0.1.5"]
+    [cascalog/carbonite "1.0.5"]
+    [riffle/riffle "0.1-dev"]
+    [log4j/log4j "1.2.16"]
+    [hadoop-util "0.2.7"]
+    [jackknife "0.1.2"]])
+
 (defproject cascalog/cascalog "1.8.6-SNAPSHOT"
   :source-path "src/clj"
   :java-source-path "src/jvm"
@@ -5,33 +22,7 @@
   :javac-options {:debug "true" :fork "true"}
   :repositories {"conjars" "http://conjars.org/repo/"}
   :codox {:include [cascalog.vars cascalog.ops cascalog.io cascalog.api]}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/tools.macro "0.1.1"]
-                 ;; jgrapht exclusion works around cascading pom bug
-                 ;; that causes projects dependent on cascalog to not
-                 ;; be able to find jgrapht.
-                 [cascading/cascading-core "1.2.4"
-                  :exclusions [org.codehaus.janino/janino
-                               thirdparty/jgrapht-jdk1.6
-                               riffle/riffle]]
-                 [thirdparty/jgrapht-jdk1.6 "0.8.1"]
-                 [cascading.kryo "0.1.5"]
-                 [cascalog/carbonite "1.0.5"]
-                 [riffle/riffle "0.1-dev"]
-                 [log4j/log4j "1.2.16"]
-                 [hadoop-util "0.2.7"]
-                 [jackknife "0.1.2"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [lein-multi "1.1.0-SNAPSHOT"]]
-  :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
-                      [cascading/cascading-core "1.2.4"
-                       :exclusions [org.codehaus.janino/janino
-                                    thirdparty/jgrapht-jdk1.6
-                                    riffle/riffle]]
-                      [thirdparty/jgrapht-jdk1.6 "0.8.1"]
-                      [cascading.kryo "0.1.5"]
-                      [cascalog/carbonite "1.0.5"]
-                      [riffle/riffle "0.1-dev"]
-                      [log4j/log4j "1.2.16"]
-                      [hadoop-util "0.2.7"]
-                      [jackknife "0.1.2"]]})
+  :dependencies  ~(conj shared-deps '[org.clojure/clojure "1.3.0"])
+  :multi-deps {"1.2" ~(conj shared-deps '[org.clojure/clojure "1.2.1"])})
