a2669daf615359104a9105c56c096dba755a2ea0
Sam Ritchie <sritchie09@gmail.com>
2012-01-19 08:43:06 -0800

    back to old wip.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def shared-deps
   '[[org.clojure/tools.macro "0.1.1"]
-    [cascading/cascading-hadoop "2.0.0-wip-187"
+    [cascading/cascading-hadoop "2.0.0-wip-184"
      :exclusions [org.codehaus.janino/janino
                   org.apache.hadoop/hadoop-core]]
     [org.clojure/tools.macro "0.1.1"]
