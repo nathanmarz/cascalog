736cd9cd9a1f2794310f5a9f087b53d5ddefec30
Sam Ritchie <sritchie09@gmail.com>
2012-04-11 15:58:54 -0700

    Added mailing list.

--- project.clj
+++ project.clj
@@ -14,8 +14,12 @@
 
 (defproject cascalog/cascalog "1.8.7-SNAPSHOT"
   :description "Hadoop without the Hassle."
+  :url "http://www.cascalog.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :mailing-list {:name "Questions and discussion about Cascalog, a Clojure-based query language for Hadoop."
+                 :archive "https://groups.google.com/d/forum/cascalog-user"
+                 :post "cascalog-user@example.org"}
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
   :jvm-opts ["-Xmx768m" "-server"]
