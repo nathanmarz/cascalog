90b78c73603f1913a720e1b8ca02afb5056e5a2a
Nathan Marz <nathan@twitter.com>
2012-04-14 00:44:56 -0700

    reimplemented ClojureOp as PredicateMacro

--- src/jvm/jcascalog/Predicate.java
+++ src/jvm/jcascalog/Predicate.java
@@ -1,39 +1,41 @@
 package jcascalog;
 
-import cascalog.Util;
 import clojure.lang.Keyword;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Predicate {
-    List<Object> _pred = new ArrayList<Object>();
-    
+    List<Object> _fieldsDeclaration = new ArrayList<Object>();
+    Object _op;
+    List<Object> _hofArgs = null;
     
     public Predicate(Object op, Fields defaultFields) {
-        addOp(_pred, op);
-        _pred.addAll(defaultFields);
+        _op = op;
+        _fieldsDeclaration.addAll(defaultFields);
     }
 
     public Predicate(Object op, Fields infields, Fields outFields) {
-        addOp(_pred, op);
-        _pred.addAll(infields);
-        _pred.add(Keyword.intern(">"));
-        _pred.addAll(outFields);
+        _op = op;
+        _fieldsDeclaration.addAll(infields);
+        _fieldsDeclaration.add(Keyword.intern(">"));
+        _fieldsDeclaration.addAll(outFields);
+    }
+    
+    public void setHofArgs(List<Object> hofArgs) {
+        _hofArgs = hofArgs;
     }
     
     public List<Object> toCascalogPredicate() {
-        return _pred;
+        List<Object> pred = new ArrayList<Object>();
+        addOp(pred, _op);
+        pred.addAll(_fieldsDeclaration);
+        return pred;
     }
     
-    private static void addOp(List<Object> pred, Object op) {
-        if(op instanceof ClojureOp) {
-            ClojureOp cop = (ClojureOp) op;
-            pred.add(Util.getVar(cop.getNamespace(), cop.getName()));
-            if(cop.getHofArgs()!=null) {
-                pred.add(cop.getHofArgs());
-            }
-        } else {
-            pred.add(op);
+    private void addOp(List<Object> pred, Object op) {
+        pred.add(op);
+        if(_hofArgs!=null) {
+            pred.add(_hofArgs);
         }
     }
 }
--- src/jvm/jcascalog/PredicateMacro.java
+++ src/jvm/jcascalog/PredicateMacro.java
@@ -1,32 +1,7 @@
 package jcascalog;
 
-import java.util.Arrays;
 import java.util.List;
 
-public class PredicateMacro {
-    Fields _inFields;
-    Fields _outFields;
-    List<Predicate> _preds;
-    
-    public PredicateMacro(Fields inFields, Fields outFields, Predicate... preds) {
-        this(inFields, outFields, Arrays.asList(preds));
-    }
-
-    public PredicateMacro(Fields inFields, Fields outFields, List<Predicate> preds) {
-        _inFields = inFields;
-        _outFields = outFields;
-        _preds = preds;
-    }
-    
-    public Fields getInFields() {
-        return _inFields;
-    }
-    
-    public Fields getOutFields() {
-        return _outFields;
-    }
-    
-    public List<Predicate> getPredicates() {
-        return _preds;
-    }
+public interface PredicateMacro {
+    List<Predicate> getPredicates(Fields inFields, Fields outFields);
 }
