d18640d0b6777c9d8349aa8ec6c465e9d6c27b9d
Nathan Marz <nathan@twitter.com>
2012-04-14 00:44:56 -0700

    reimplemented ClojureOp as PredicateMacro

--- src/jvm/jcascalog/ClojureOp.java
+++ src/jvm/jcascalog/ClojureOp.java
@@ -2,9 +2,11 @@ package jcascalog;
 
 // TODO: implement as part of transformation to real predicates
 
+import cascalog.Util;
+import java.util.Arrays;
 import java.util.List;
 
-public class ClojureOp {
+public class ClojureOp implements PredicateMacro {
     String _namespace;
     String _name;
     List<Object> _hofArgs;
@@ -18,18 +20,11 @@ public class ClojureOp {
     public ClojureOp(String namespace, String name) {
         this(namespace, name, null);
     }
-    
-    public String getNamespace() {
-        return _namespace;
+
+    @Override
+    public List<Predicate> getPredicates(Fields inFields, Fields outFields) {
+        Predicate pred = new Predicate(Util.getVar(_namespace, _name), inFields, outFields);
+        pred.setHofArgs(_hofArgs);
+        return Arrays.asList(pred);
     }
-    
-    public String getName() {
-        return _name;
-    }
-    
-    public List<Object> getHofArgs() {
-        return _hofArgs;
-    }
-    
-    
 }
