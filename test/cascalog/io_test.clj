301a558d27e317b745a417924a71d42cd0d749c7
Stefan Hübner <sthuebner@googlemail.com>
2012-07-17 10:46:24 +0200

    tests for conf/with-fs-tmp

---
+++ test/cascalog/io_test.clj
@@ -0,0 +1,16 @@
+(ns cascalog.io-test
+  (:use cascalog.io
+        midje.sweet
+        clojure.test)
+  (:require [cascalog.api :as api]))
+
+(deftest configurable-with-fs-tmp
+ (is (.startsWith (with-fs-tmp [_ foo]
+                    foo)
+                  "/tmp/cascalog_reserved/"))
+ (is (.startsWith (api/with-job-conf {"cascalog.tmpdir"
+                                      ;; deliberately using lein's build directory
+                                      "target/bar"}
+                    (with-fs-tmp [_ foo]
+                      foo))
+                  "target/bar/cascalog_reserved/")))
