d0ef46bc80ccd00ad6d79e9e0c1b5d3632f18dff
Soren Macbeth <soren@dopeness.org>
2013-01-04 22:55:35 -0800

    fix stay merge conflicts

--- src/clj/cascalog/workflow.clj
+++ src/clj/cascalog/workflow.clj
@@ -62,16 +62,8 @@
    (and (sequential? obj) (every? string? obj))))
 
 (defn parse-args
-<<<<<<< variant A
   "arr => op in-fields? :fn> func-fields :> out-fields
   
->>>>>>> variant B
-  "arr => func-spec in-fields? :fn> func-fields :> out-fields
-
-####### Ancestor
-  "arr => func-spec in-fields? :fn> func-fields :> out-fields
-  
-======= end
   returns [in-fields func-fields spec out-fields]"
   ([arr] (parse-args arr Fields/RESULTS))
   ([[op & varargs] defaultout]
@@ -261,7 +253,6 @@
               (fields declared-fields)
               joiner)))
 
-<<<<<<< variant A
 (defn ophelper [type builder afn]
   (u/merge-meta afn {::op-builder builder :pred-type type}))
 
@@ -320,237 +311,6 @@
   (let [builder (get (meta op) ::op-builder filter)]
     (apply builder op args)
     ))
->>>>>>> variant B
-(defn- update-arglists
-  "Scans the forms of a def* operation and adds an appropriate
-  `:arglists` entry to the supplied `sym`'s metadata."
-  [sym [form :as args]]
-  (let [arglists (if (vector? form)
-                   (list form)
-                   (clojure.core/map clojure.core/first args))]
-    (u/meta-conj sym {:arglists (list 'quote arglists)})))
-
-(defn- update-fields
-  "Examines the first item in a def* operation's forms. If the first
-  form defines a sequence of Cascading output fields, these are added
-  to the supplied `sym`'s metadata and dropped from the form
-  sequence. Else, `sym` and `forms` are left unchanged.
-
-  This function will no longer be necessary, if Cascalog deprecates
-  the ability to name output fields before the dynamic argument
-  vector."
-  [sym [form :as forms]]
-  (if (string? (clojure.core/first form))
-    [(u/meta-conj sym {:fields form}) (rest forms)]
-    [sym forms]))
-
-(defn assert-nonvariadic [args]
-  (safe-assert (not (some #{'&} args))
-               (str "Defops currently don't support variadic arguments.\n"
-                    "The following argument vector is invalid: " args)))
-
-(defn- parse-defop-args
-  "Accepts a def* type and the body of a def* operation binding,
-  outfits the function var with all appropriate metadata, and returns
-  a 3-tuple containing `[fname f-args args]`.
-
-  * `fname`: the function var.
-  * `f-args`: static variable declaration vector.
-  * `args`: dynamic variable declaration vector."
-  [type [spec & args]]
-  (let [[fname f-args] (if (sequential? spec)
-                         [(clojure.core/first spec) (second spec)]
-                         [spec nil])
-        [fname args] (->> [fname args]
-                          (apply name-with-attributes)
-                          (apply update-fields))
-        fname (update-arglists fname args)
-        fname (u/meta-conj fname {:pred-type (keyword (name type))
-                                  :hof? (boolean f-args)})]
-    (assert-nonvariadic f-args)
-    [fname f-args args]))
-
-(defn- defop-helper
-  "Binding helper for cascalog def* ops. Function value is tagged with
-   appropriate cascalog metadata; metadata can be accessed with `(meta
-   op)`, rather than the previous `(op :meta)` requirement. This is so
-   you can pass operations around and dynamically create flows."
-  [type args]
-  (let  [[fname func-args funcdef] (parse-defop-args type args)
-         args-sym        (gensym "args")
-         args-sym-all    (gensym "argsall")
-         runner-name     (symbol (str fname "__"))
-         func-form       (if func-args
-                           `[(var ~runner-name) ~@func-args]
-                           `(var ~runner-name))
-         runner-body     (if func-args
-                           `(~func-args (fn ~@funcdef))
-                           funcdef)
-         assembly-args   (if func-args
-                           `[~func-args & ~args-sym]
-                           `[ & ~args-sym])]
-    `(do (defn ~runner-name
-           ~(assoc (meta fname)
-              :no-doc true
-              :skip-wiki true)
-           ~@runner-body)
-         (def ~fname
-           (with-meta
-             (fn [& ~args-sym-all]
-               (let [~assembly-args ~args-sym-all]
-                 (apply ~type ~func-form ~args-sym)))
-             ~(meta fname))))))
-
-(defmacro defmapop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/map args))
-
-(defmacro defmapcatop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/mapcat args))
-
-(defmacro deffilterop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/filter args))
-
-(defmacro defaggregateop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/aggregate args))
-
-(defmacro defbufferop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/buffer args))
-
-(defmacro defmultibufferop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/multibuffer args))
-
-(defmacro defbufferiterop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/bufferiter args))
-####### Ancestor
-(defn- update-arglists
-  "Scans the forms of a def* operation and adds an appropriate
-  `:arglists` entry to the supplied `sym`'s metadata."
-  [sym [form :as args]]
-  (let [arglists (if (vector? form)
-                   (list form)
-                   (clojure.core/map clojure.core/first args))]
-    (u/meta-conj sym {:arglists (list 'quote arglists)})))
-
-(defn- update-fields
-  "Examines the first item in a def* operation's forms. If the first
-  form defines a sequence of Cascading output fields, these are added
-  to the supplied `sym`'s metadata and dropped from the form
-  sequence. Else, `sym` and `forms` are left unchanged.
-
-  This function will no longer be necessary, if Cascalog deprecates
-  the ability to name output fields before the dynamic argument
-  vector."
-  [sym [form :as forms]]
-  (if (string? (clojure.core/first form))
-    [(u/meta-conj sym {:fields form}) (rest forms)]
-    [sym forms]))
-
-(defn assert-nonvariadic [args]
-  (safe-assert (not (some #{'&} args))
-               (str "Defops currently don't support variadic arguments.\n"
-                    "The following argument vector is invalid: " args)))
-
-(defn- parse-defop-args
-  "Accepts a def* type and the body of a def* operation binding,
-  outfits the function var with all appropriate metadata, and returns
-  a 3-tuple containing `[fname f-args args]`.
-
-  * `fname`: the function var.
-  * `f-args`: static variable declaration vector.
-  * `args`: dynamic variable declaration vector."
-  [type [spec & args]]  
-  (let [[fname f-args] (if (sequential? spec)
-                         [(clojure.core/first spec) (second spec)]
-                         [spec nil])
-        [fname args] (->> [fname args]
-                          (apply name-with-attributes)
-                          (apply update-fields))
-        fname (update-arglists fname args)
-        fname (u/meta-conj fname {:pred-type (keyword (name type))
-                                  :hof? (boolean f-args)})]
-    (assert-nonvariadic f-args)
-    [fname f-args args]))
-
-(defn- defop-helper
-  "Binding helper for cascalog def* ops. Function value is tagged with
-   appropriate cascalog metadata; metadata can be accessed with `(meta
-   op)`, rather than the previous `(op :meta)` requirement. This is so
-   you can pass operations around and dynamically create flows."
-  [type args]
-  (let  [[fname func-args funcdef] (parse-defop-args type args)
-         args-sym        (gensym "args")
-         args-sym-all    (gensym "argsall")
-         runner-name     (symbol (str fname "__"))
-         func-form       (if func-args
-                           `[(var ~runner-name) ~@func-args]
-                           `(var ~runner-name))
-         runner-body     (if func-args
-                           `(~func-args (fn ~@funcdef))
-                           funcdef)
-         assembly-args   (if func-args
-                           `[~func-args & ~args-sym]
-                           `[ & ~args-sym])]
-    `(do (defn ~runner-name
-           ~(assoc (meta fname)
-              :no-doc true
-              :skip-wiki true)
-           ~@runner-body)
-         (def ~fname
-           (with-meta
-             (fn [& ~args-sym-all]
-               (let [~assembly-args ~args-sym-all]
-                 (apply ~type ~func-form ~args-sym)))
-             ~(meta fname))))))
-
-(defmacro defmapop  
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/map args))
-
-(defmacro defmapcatop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/mapcat args))
-
-(defmacro deffilterop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/filter args))
-
-(defmacro defaggregateop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/aggregate args))
-
-(defmacro defbufferop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/buffer args))
-
-(defmacro defmultibufferop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/multibuffer args))
-
-(defmacro defbufferiterop
-  {:arglists '([name doc-string? attr-map? [fn-args*] body])}
-  [& args]
-  (defop-helper 'cascalog.workflow/bufferiter args))
-======= end
 
 (defn assemble
   ([x] x)
@@ -684,11 +444,6 @@ identity.  identity."
 (defn write-dot [^Flow flow ^String path]
   (.writeDOT flow path))
 
-<<<<<<< variant A
-(defn fill-tap! [^Tap tap xs] 
-  (with-open [^TupleEntryCollector collector (.openForWrite tap (hadoop/job-conf
-                                                                 (conf/project-conf)))]
->>>>>>> variant B
 (defn exec [^Flow flow]
   (.complete flow))
 
@@ -697,14 +452,6 @@ identity.  identity."
               (-> (hadoop/job-conf (conf/project-conf))
                   (HadoopFlowProcess.)
                   (.openTapForWrite tap))]
-####### Ancestor
-(defn exec [^Flow flow]
-  (.complete flow))
-
-(defn fill-tap! [^Tap tap xs] 
-  (with-open [^TupleEntryCollector collector (.openForWrite tap (hadoop/job-conf
-                                                                 (conf/project-conf)))]
-======= end
     (doseq [item xs]
       (.add collector (Util/coerceToTuple item)))))
 
