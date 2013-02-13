f3e7c4bcc7a1a5be5be82cf270055ec445ff2677
Soren Macbeth <soren@dopeness.org>
2013-02-13 08:39:11 -0800

    fix up cascalog-core project.clj

--- cascalog-core/project.clj
+++ cascalog-core/project.clj
@@ -6,28 +6,28 @@
   :jvm-opts ["-Xmx768m"
              "-server"
              "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n"]
-  :javac-options {:target "1.6" :source "1.6"}
+  :javac-options ["-target" "1.6" "-source" "1.6"]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :repositories {"conjars" "http://conjars.org/repo/"}
-  :plugins [[lein-midje "3.0-alpha4"]]
   :exclusions [log4j/log4j org.slf4j/slf4j-log4j12]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.macro "0.1.1"]
                  [log4j "1.2.16"]
                  [org.slf4j/slf4j-log4j12 "1.6.6"]
-                 [cascading/cascading-hadoop "2.0.3"
+                 [cascading/cascading-hadoop "2.0.6"
                   :exclusions [org.codehaus.janino/janino
                                org.apache.hadoop/hadoop-core]]
-                 [cascading.kryo "0.4.5"]
+                 [cascading.kryo "0.4.6"]
                  [com.twitter/carbonite "1.3.1"]
                  [com.twitter/maple "0.2.2"]
                  [jackknife "0.1.2"]
-                 [hadoop-util "0.2.8"]]
+                 [hadoop-util "0.2.9"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}
              :dev {:resource-paths ["dev"]
+                   :plugins [[lein-midje "3.0-alpha4"]]
                    :dependencies
-                   [[org.apache.hadoop/hadoop-core "1.0.3"]
-                    [midje "1.5-alpha10"]
-                    [midje-cascalog "0.4.0" :exclusions [org.clojure/clojure]]]}})
+                   [[midje "1.5-alpha10"]
+                    [midje-cascalog "0.4.0" :exclusions [org.clojure/clojure]]]}
+             :provided {:dependencies [[org.apache.hadoop/hadoop-core "1.0.3"]]}})
