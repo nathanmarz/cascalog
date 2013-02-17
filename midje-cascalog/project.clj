dae4c1307cb07b95972da7fe03adfd57d7594dc0
Paul Lam <paul@quantisan.com>
2013-02-17 16:35:28 +0000

    fix midje project.clj deps, wrap midje tests in deftest

--- midje-cascalog/project.clj
+++ midje-cascalog/project.clj
@@ -4,8 +4,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"conjars.org" "http://conjars.org/repo"}
   :dependencies [[cascalog/cascalog-core "1.10.1-SNAPSHOT"]
-                 [midje "1.5-beta1"]]
-  :plugins [[lein-midje "3.0-beta1"]]
+                 [midje "1.3.0"]]
+   :plugins [[lein-midje "2.0.4"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}
              :dev {:dependencies
