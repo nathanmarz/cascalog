a939513957c23b0a6aa67c7e8061a87127010006
Soren Macbeth <soren@dopeness.org>
2013-02-28 15:00:52 -0800

    bump to 0.4.0-RC1

--- cascalog-elephantdb/project.clj
+++ cascalog-elephantdb/project.clj
@@ -6,12 +6,12 @@
   :javac-options ["-source" "1.6" "-target" "1.6"]
   :jvm-opts ["-server" "-Xmx768m"]
   :dependencies [[cascalog/cascalog-core "1.10.1-SNAPSHOT"]
-                 [elephantdb/elephantdb-cascading "0.4.0-SNAPSHOT"]]
+                 [elephantdb/elephantdb-cascading "0.4.0-RC1"]]
   :plugins [[lein-midje "3.0-alpha4"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}
              :dev {:dependencies
-                   [[elephantdb/elephantdb-bdb "0.4.0-SNAPSHOT"]
+                   [[elephantdb/elephantdb-bdb "0.4.0-RC1"]
                     [midje "1.5-alpha10"]
                     [midje-cascalog "0.4.0"
                      :exclusions [org.clojure/clojure]]]}
