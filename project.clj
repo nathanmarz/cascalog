4a52a79262af926200131e0b1918a996ef96d970
Soren Macbeth <soren@dopeness.org>
2013-03-15 07:07:02 -0700

    remove cascalog-lzo from project.clj for now

--- project.clj
+++ project.clj
@@ -10,7 +10,6 @@
                  [cascalog/cascalog-checkpoint "1.10.1-SNAPSHOT"]
                  [cascalog/cascalog-more-taps "1.10.1-SNAPSHOT"]
                  [cascalog/cascalog-elephantdb "1.10.1-SNAPSHOT"]
-                 [cascalog/cascalog-lzo "1.10.1-SNAPSHOT"]
                  [cascalog/cascalog-math "1.10.1-SNAPSHOT"]
                  [cascalog/midje-cascalog "1.10.1-SNAPSHOT"]]
   :plugins [[lein-sub "0.2.1"]
@@ -19,7 +18,6 @@
         "cascalog-checkpoint"
         "cascalog-more-taps"
         "cascalog-elephantdb"
-        "cascalog-lzo"
         "cascalog-math"
         "midje-cascalog"]
   :codox {:src-dir-uri "http://github.com/nathanmarz/cascalog/blob/master"
@@ -28,6 +26,5 @@
                     "cascalog-checkpoint/src"
                     "cascalog-more-taps/src"
                     "cascalog-elephantdb/src"
-                    "cascalog-lzo/src"
                     "cascalog-math/src"
                     "midje-cascalog/src"]})
