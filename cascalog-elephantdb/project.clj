3563bf0777ebc9b64fa164fe90aba55b1709cf48
Soren Macbeth <soren@dopeness.org>
2013-04-08 15:35:35 -0700

    bump to elephantdb-cascading 0.4.1-SNAPSHOT

--- cascalog-elephantdb/project.clj
+++ cascalog-elephantdb/project.clj
@@ -5,17 +5,17 @@
   :source-paths ["src/clj"]
   :javac-options ["-source" "1.6" "-target" "1.6"]
   :jvm-opts ["-server" "-Xmx768m"]
-  :dependencies [[elephantdb/elephantdb-cascading "0.4.0"]]
+  :dependencies [[elephantdb/elephantdb-cascading "0.4.1-SNAPSHOT"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :provided {:dependencies [[cascalog/cascalog-core "1.10.2-SNAPSHOT"]]}
              :dev {:dependencies
-                   [[elephantdb/elephantdb-bdb "0.4.0"]
+                   [[elephantdb/elephantdb-bdb "0.4.1-SNAPSHOT"]
                     [org.apache.hadoop/hadoop-core "0.20.2"]
                     [cascalog/midje-cascalog "1.10.2-SNAPSHOT"]]
                    :plugins [[lein-midje "3.0.0"]]}
              :ci-dev {:dependencies
-                      [[elephantdb/elephantdb-bdb "0.4.0"]
+                      [[elephantdb/elephantdb-bdb "0.4.1-SNAPSHOT"]
                        [org.apache.hadoop/hadoop-core "1.0.3"]
                        [cascalog/midje-cascalog "1.10.2-SNAPSHOT"]]
                       :plugins [[lein-midje "3.0.0"]]}})
