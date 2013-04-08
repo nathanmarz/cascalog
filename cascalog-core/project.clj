b2098662d421aa624920ee963925c5339743ad0a
Soren Macbeth <soren@dopeness.org>
2013-04-08 15:35:14 -0700

    bump to carbonite 1.3.2

--- cascalog-core/project.clj
+++ cascalog-core/project.clj
@@ -19,7 +19,7 @@
                   :exclusions [org.codehaus.janino/janino
                                org.apache.hadoop/hadoop-core]]
                  [cascading.kryo "0.4.6"]
-                 [com.twitter/carbonite "1.3.1"]
+                 [com.twitter/carbonite "1.3.2"]
                  [com.twitter/maple "0.2.2"]
                  [jackknife "0.1.2"]
                  [hadoop-util "0.2.9"]]
