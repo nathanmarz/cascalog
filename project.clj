17bc18976fb329b1b49d059799ce46dc9ebdd968
Soren Macbeth <soren@dopeness.org>
2013-05-10 11:32:29 -0700

    cascalog-elephantdb has moved to github.com/nathanmarz/elephantdb
    and renamed to elephantdb-cascalog

--- project.clj
+++ project.clj
@@ -9,7 +9,6 @@
   :dependencies [[cascalog/cascalog-core "1.10.2-SNAPSHOT"]
                  [cascalog/cascalog-checkpoint "1.10.2-SNAPSHOT"]
                  [cascalog/cascalog-more-taps "1.10.2-SNAPSHOT"]
-                 [cascalog/cascalog-elephantdb "1.10.2-SNAPSHOT"]
                  [cascalog/cascalog-math "1.10.2-SNAPSHOT"]
                  [cascalog/midje-cascalog "1.10.2-SNAPSHOT"]]
   :plugins [[lein-sub "0.2.1"]
@@ -17,7 +16,6 @@
   :sub ["cascalog-core"
         "cascalog-checkpoint"
         "cascalog-more-taps"
-        "cascalog-elephantdb"
         "cascalog-math"
         "midje-cascalog"]
   :codox {:src-dir-uri "http://github.com/nathanmarz/cascalog/blob/master"
@@ -25,6 +23,5 @@
           :sources ["cascalog-core/src"
                     "cascalog-checkpoint/src"
                     "cascalog-more-taps/src"
-                    "cascalog-elephantdb/src"
                     "cascalog-math/src"
                     "midje-cascalog/src"]})
