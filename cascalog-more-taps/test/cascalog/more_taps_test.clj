3b3a7594a4db93e87c2abe261a7d3146bf9eb768
Paul Lam <paul@quantisan.com>
2013-06-09 11:01:07 +0100

    more-taps inner quotes read and write tests

--- cascalog-more-taps/test/cascalog/more_taps_test.clj
+++ cascalog-more-taps/test/cascalog/more_taps_test.clj
@@ -33,3 +33,23 @@
         (<- [?a ?b ?c]
             ((hfs-delimited tmp :delimiter "," :compress? true) ?a ?b ?c)) =>
         (produces [["Proin" "false" "3"]])))))
+
+(deftest inner-quotes-read-test
+  (fact
+    (io/with-fs-tmp [_ tmp]
+      (?- (hfs-textline tmp)  ;; write line
+          [["Proin,false,3,\"hello \"there\"!\""]])
+      (fact
+        (<- [?a ?b ?c ?d]
+            ((hfs-delimited tmp :delimiter "," :quote "\"") ?a ?b ?c ?d)) =>
+        (produces [["Proin" "false" "3" "hello \"there\"!"]])))))
+
+(deftest inner-quotes-write-test
+  (fact
+    (io/with-fs-tmp [_ tmp]
+      (?- (hfs-delimited tmp :delimiter "," :quote "\"")
+          [["Proin" "false" "3" "\"hello, \"there\"!\""]])
+      (fact
+        (<- [?line]
+            ((hfs-textline tmp) ?line))
+        (produces [["Proin,false,3,\"hello, \"there\"!\""]])))))
