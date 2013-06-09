5916d075877ca643473acc62cf443df8aa52e6b9
Paul Lam <paul@quantisan.com>
2013-06-09 10:35:32 +0100

    wrap more-taps tests in deftest so they run in CI

--- cascalog-more-taps/test/cascalog/more_taps_test.clj
+++ cascalog-more-taps/test/cascalog/more_taps_test.clj
@@ -1,31 +1,35 @@
 (ns cascalog.more-taps-test
-  (:use [cascalog api more-taps]
+  (:use [clojure.test]
+        [cascalog api more-taps]
         [midje sweet cascalog])
   (:require [cascalog.io :as io]))
 
-(fact
-  (io/with-fs-tmp [_ tmp]
-    (?- (hfs-textline tmp)   ;; write line
-        [["Proin,hendrerit,tincidunt pellentesque"]])
-    (fact "Test round trip"
-      (<- [?a ?b ?c]
-          ((hfs-delimited tmp :delimiter ",") ?a ?b ?c)) =>
-      (produces [["Proin" "hendrerit" "tincidunt pellentesque"]]))))
+(deftest roundtrip-test
+  (fact
+    (io/with-fs-tmp [_ tmp]
+      (?- (hfs-textline tmp)   ;; write line
+          [["Proin,hendrerit,tincidunt pellentesque"]])
+      (fact "Test round trip"
+        (<- [?a ?b ?c]
+            ((hfs-delimited tmp :delimiter ",") ?a ?b ?c)) =>
+        (produces [["Proin" "hendrerit" "tincidunt pellentesque"]])))))
 
-(fact
-  (io/with-fs-tmp [_ tmp]
-    (?- (hfs-textline tmp)   ;; write line
-        [["Proin,false,3"]])
-    (fact "Classes"
-      (<- [?a ?b ?c]
-          ((hfs-delimited tmp :delimiter "," :classes [String Boolean Integer]) ?a ?b ?c)) =>
-      (produces [["Proin" false 3]]))))
+(deftest specify-classes-test
+  (fact
+    (io/with-fs-tmp [_ tmp]
+      (?- (hfs-textline tmp)   ;; write line
+          [["Proin,false,3"]])
+      (fact "Classes"
+        (<- [?a ?b ?c]
+            ((hfs-delimited tmp :delimiter "," :classes [String Boolean Integer]) ?a ?b ?c)) =>
+        (produces [["Proin" false 3]])))))
 
-(fact
-  (io/with-fs-tmp [_ tmp]
-    (?- (hfs-delimited tmp :delimiter "," :compress? true)   ;; write line
-        [["Proin" false 3]])
-    (fact "Compression"
-      (<- [?a ?b ?c]
-          ((hfs-delimited tmp :delimiter "," :compress? true) ?a ?b ?c)) =>
-      (produces [["Proin" "false" "3"]]))))
+(deftest compress-test
+  (fact
+    (io/with-fs-tmp [_ tmp]
+      (?- (hfs-delimited tmp :delimiter "," :compress? true)   ;; write line
+          [["Proin" false 3]])
+      (fact "Compression"
+        (<- [?a ?b ?c]
+            ((hfs-delimited tmp :delimiter "," :compress? true) ?a ?b ?c)) =>
+        (produces [["Proin" "false" "3"]])))))
