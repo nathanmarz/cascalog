4bcea9e10a06eb541ac5cb1f31503ba0f552151e
Sam Ritchie <sritchie@twitter.com>
2013-07-17 00:00:38 -0400

    more tests

--- cascalog-core/test/cascalog/logic/pred_macro_test.clj
+++ cascalog-core/test/cascalog/logic/pred_macro_test.clj
@@ -113,124 +113,123 @@
              (nums !a !b)
              ((c/juxt #'+ #'- #'<) !a !b :> !v1 !v2 !v3))))
 
-(comment
-  (defn splitter [^String str]
-    (seq (.split str " ")))
+(defn splitter [^String str]
+  (seq (.split str " ")))
 
-  (deftest test-destructuring
-    (let [triplets [[1 2 3] [2 3 4] [3 3 5]]
-          pairs    [["a b"] ["c d"]]
-          singles  [["a"] ["b"]]
-          str-mac (<- [:<< !all :> !val]
-                      (str :<< !all :> !i)
-                      (str !i "a" :> !val))
-          complex-mac (<- [:<< [!a !b & !rest :as !all] :>> [!v1 !v2 !v3]]
-                          (str :<< !all :> !v1)
-                          (str !a !b :> !v2)
-                          (str :<< !rest :> !v3))
-          var-out (<- [!a :>> !allout]
-                      (str !a "a" :> !b)
-                      (splitter !b :>> !allout))]
-      (test?<- [["123a"] ["234a"] ["335a"]]
-               [!out]
-               (triplets !a !b !c)
-               (str-mac !a !b !c :> !out))
+(deftest test-destructuring
+  (let [triplets [[1 2 3] [2 3 4] [3 3 5]]
+        pairs    [["a b"] ["c d"]]
+        singles  [["a"] ["b"]]
+        str-mac (<- [:<< !all :> !val]
+                    (str :<< !all :> !i)
+                    (str !i "a" :> !val))
+        complex-mac (<- [:<< [!a !b & !rest :as !all] :>> [!v1 !v2 !v3]]
+                        (str :<< !all :> !v1)
+                        (str !a !b :> !v2)
+                        (str :<< !rest :> !v3))
+        var-out (<- [!a :>> !allout]
+                    (str !a "a" :> !b)
+                    (splitter !b :>> !allout))]
+    (test?<- [["123a"] ["234a"] ["335a"]]
+             [!out]
+             (triplets !a !b !c)
+             (str-mac !a !b !c :> !out))
 
-      (test?<- [["32a"] ["43a"] ["53a"]]
-               [!out]
-               (triplets _ !b !c)
-               (str-mac !c !b :> !out))
+    (test?<- [["32a"] ["43a"] ["53a"]]
+             [!out]
+             (triplets _ !b !c)
+             (str-mac !c !b :> !out))
 
-      (test?<- [["123" "12" "3"] ["234" "23" "4"] ["335" "33" "5"]]
-               [!o1 !o2 !o3]
-               (triplets !a !b !c)
-               (complex-mac !a !b !c :> !o1 !o2 !o3))
+    (test?<- [["123" "12" "3"] ["234" "23" "4"] ["335" "33" "5"]]
+             [!o1 !o2 !o3]
+             (triplets !a !b !c)
+             (complex-mac !a !b !c :> !o1 !o2 !o3))
 
-      (future-fact
-       "this test doesn't work because cascading doesn't allow
+    (future-fact
+     "this test doesn't work because cascading doesn't allow
       functions with no input"
-       (test?<- [["12" ""] ["23" ""] ["33" ""]]
-                [!o2 !o3]
-                (triplets !a !b _)
-                (complex-mac !a !b :> !o1 !o2 !o3)))
+     (test?<- [["12" ""] ["23" ""] ["33" ""]]
+              [!o2 !o3]
+              (triplets !a !b _)
+              (complex-mac !a !b :> !o1 !o2 !o3)))
 
-      (test?<- [["12" "12"] ["23" "23"] ["33" "33"]]
-               [!o1 !o2]
+    (test?<- [["12" "12"] ["23" "23"] ["33" "33"]]
+             [!o1 !o2]
 
-               (triplets !a !b _)
-               (complex-mac !a !b :> !o1 !o2 !o3))
+             (triplets !a !b _)
+             (complex-mac !a !b :> !o1 !o2 !o3))
 
-      (test?<- [["a" "ba"] ["c" "da"]]
-               [!a !b]
-               (pairs !i)
-               (var-out !i :> !a !b))
+    (test?<- [["a" "ba"] ["c" "da"]]
+             [!a !b]
+             (pairs !i)
+             (var-out !i :> !a !b))
 
-      (test?<- [["aa"] ["ba"]]
-               [!a]
-               (singles !i)
-               (var-out !i :> !a))))
+    (test?<- [["aa"] ["ba"]]
+             [!a]
+             (singles !i)
+             (var-out !i :> !a))))
 
-  (future-fact " TODO: test construct with destructuring, esp. string &")
+(future-fact " TODO: test construct with destructuring, esp. string &")
 
 
-  (defn append-! [v]
-    (str v "!"))
+(defn append-! [v]
+  (str v "!"))
 
-  (deffilterop small-op? [v]
-    (< v 4))
+(deffilterop small-op? [v]
+  (< v 4))
 
-  (deftest test-each
-    (let [triples [[1 2 3] [3 4 1]]]
-      (test?<-[["1!" "2!"] ["3!" "4!"]]
-              [!v1 !v2]
-              (triples !a !b !c)
-              ((c/each #'append-!) !a !b :> !v1 !v2)
-              (:distinct false))
+(deftest test-each
+  (let [triples [[1 2 3] [3 4 1]]]
+    (test?<-[["1!" "2!"] ["3!" "4!"]]
+            [!v1 !v2]
+            (triples !a !b !c)
+            ((c/each #'append-!) !a !b :> !v1 !v2)
+            (:distinct false))
 
-      (test?<-[["2!"] ["4!"]]
-              [!v]
-              (triples !a !b !c)
-              ((c/each #'append-!) !b :> !v)
-              (:distinct false))
+    (test?<-[["2!"] ["4!"]]
+            [!v]
+            (triples !a !b !c)
+            ((c/each #'append-!) !b :> !v)
+            (:distinct false))
 
-      (test?<-[[3 1 2]]
-              [!c !a !b]
-              (triples !a !b !c)
-              ((c/each small-op?) !a !b !c)
-              (:distinct false))))
+    (test?<-[[3 1 2]]
+            [!c !a !b]
+            (triples !a !b !c)
+            ((c/each small-op?) !a !b !c)
+            (:distinct false))))
 
-  (deftest test-nested-predmacro
-    (let [integers [[1] [4]]
-          pm1 (predmacro [invars outvars]
-                         (map (fn [i v] [#'append-! i :> v]) invars outvars))
-          pm2 (predmacro [invars outvars]
-                         [[pm1 :<< invars :>> outvars]
-                          [small-op? (first invars)]])]
-      (test?<- [["1!"]]
-               [?v]
-               (integers ?i)
-               (pm2 ?i :> ?v))))
+(deftest test-nested-predmacro
+  (let [integers [[1] [4]]
+        pm1 (predmacro [invars outvars]
+                       (map (fn [i v] [#'append-! i :> v]) invars outvars))
+        pm2 (predmacro [invars outvars]
+                       [[pm1 :<< invars :>> outvars]
+                        [small-op? (first invars)]])]
+    (test?<- [["1!"]]
+             [?v]
+             (integers ?i)
+             (pm2 ?i :> ?v))))
 
-  (defn small? [num]
+(defn small? [num]
     (< num 10))
 
-  (deftest test-composite-composites
-    (let [nums [[1 2] [3 3] [4 6] [6 8] [-2 -1]]]
-      (test?<- [[4] [-2]]
-               [!a]
-               (nums !a _)
-               ((c/negate (c/any #'odd? #'mult-3-sum?)) !a))
+(deftest test-composite-composites
+  (let [nums [[1 2] [3 3] [4 6] [6 8] [-2 -1]]]
+    (test?<- [[4] [-2]]
+             [!a]
+             (nums !a _)
+             ((c/negate (c/any #'odd? #'mult-3-sum?)) !a))
 
-      (test?<- [[3] [4] [6]]
-               [!a]
-               (nums !a _)
-               ((c/any
-                 (c/all #'odd? #'mult-3-sum?)
-                 (c/all #'even? #'pos? #'small?)) !a))
-      (future-fact "TODO: not all any")))
+    (test?<- [[3] [4] [6]]
+             [!a]
+             (nums !a _)
+             ((c/any
+               (c/all #'odd? #'mult-3-sum?)
+               (c/all #'even? #'pos? #'small?)) !a))
 
-  (deftest test-errors
-    (thrown?<- RuntimeException
-               [?a ?b ?c :> ?d ?a]
-               (+ ?a ?b ?c :> ?d)))
-  )
+    (future-fact "TODO: not all any")))
+
+(deftest test-errors
+  (thrown?<- RuntimeException
+             [?a ?b ?c :> ?d ?a]
+             (+ ?a ?b ?c :> ?d)))
