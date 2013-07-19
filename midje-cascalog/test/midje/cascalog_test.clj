c76f8e1d406c96af624ea7c9ee8e6dedd6883574
Paul Lam <paul@quantisan.com>
2013-07-19 14:37:47 +0100

    midje-cascalog passing, removed complex queries to be added into core as test

--- midje-cascalog/test/midje/cascalog_test.clj
+++ midje-cascalog/test/midje/cascalog_test.clj
@@ -102,7 +102,7 @@
     (a-query [[10]]) => (produces [[10]])
     (<- [?a ?b]
         ([[10 11] [12 13]] ?a ?b)
-        ([[11]] ?b :> true)) => (produces [[10 11]])
+        ([[11]] ?b)) => (produces [[10 11]])
     (against-background
       (whoop) => [[10 11] [12 13]]
       (bang)  => [[11]]))
@@ -125,7 +125,6 @@
                [5 2] [5 9]]
         query (<- [?x ?sum]
                   (src ?x ?y)
-                  (:sort ?x)
                   (c/sum ?y :> ?sum))]
     (facts
       "Executing the query produces proper sums in either order."
