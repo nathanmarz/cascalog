3096ff479f34a96ed5e1895372c0f786076b2f7d
Paul Lam <paul@quantisan.com>
2013-07-19 15:18:52 +0100

    update checkpoint ns, test pass

--- cascalog-checkpoint/project.clj
+++ cascalog-checkpoint/project.clj
@@ -5,8 +5,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"conjars.org" "http://conjars.org/repo"}
-  :dependencies [[jackknife "0.1.2"]
-                 [hadoop-util "0.2.8"]]
+  :dependencies [[jackknife "0.1.3"]
+                 [hadoop-util "0.3.0"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :provided {:dependencies [[cascalog/cascalog-core ~cascalog-version]]}
