f20149fea93f9af6c12e4297066edcbb77543492
Paul Lam <paul@quantisan.com>
2013-10-09 11:53:30 +0100

    deps tree cleanup in midje to fix clojure ver conflict

--- midje-cascalog/project.clj
+++ midje-cascalog/project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :repositories {"conjars.org" "http://conjars.org/repo"}
-  :dependencies [[midje "1.5.1"]]
+  :dependencies [[midje "1.5.1" :exclusions [org.clojure/clojure]]]
   :plugins [[lein-midje "3.0.0"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
