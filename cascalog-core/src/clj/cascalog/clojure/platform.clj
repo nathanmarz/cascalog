aef94156d2679e50af989fdc8478dfd8a9caff82
bhiles <bennetthiles@gmail.com>
2014-07-15 16:54:36 -0700

    cleaned up functions and added Application IRunner

--- cascalog-core/src/clj/cascalog/clojure/platform.clj
+++ cascalog-core/src/clj/cascalog/clojure/platform.clj
@@ -7,37 +7,51 @@
            [cascalog.logic.predicate Generator RawSubquery]))
 
 ;; Generator
-(defn name-tuples
-  [names tuples]
-  (map
-   (fn [tup]
-       (zipmap names tup))
-   tuples))
+(defn to-tuple
+  [names v]
+  (zipmap names v))
+
+(defn to-tuples
+  "turns [\"n\"] and [[1] [2]] into [{\"n\" 1} {\"n\" 2}]"
+  [names coll-of-seqs]
+  (map #(to-tuple names %) coll-of-seqs))
+
+(defn select-fields
+  "Creates a list of the values of the tuples you want.
+   For examples: (select-fields [:b :a] {:a 1 :b 2 :c 3}) => (2 1)"
+  [fields tuple]
+  (map #(tuple %) fields))
 
 ;; Projection
-(defn select-fields
+(defn extract-fields
+  "Creates a collection of vectors for the fields you have selected"
   [fields tuples]  
   (remove nil?
-          (map
-           (fn [tup]
-               (map
-                (fn [k]
-                    (tup k))
-                fields)
-               )
-           tuples)))		
+          (map #(select-fields fields %)
+               tuples)))
 
 (defprotocol IRunner
   (to-generator [item]))
 
+;; Extend these types to allow for different types of querying: joins,
+;; filters, aggregation, etc.  You need to extend all the types that
+;; implement defnode in logic.parse, otherwise you won't implement the full query capabilities
 (extend-protocol IRunner
   Projection
   (to-generator [{:keys [source fields]}]
-    (select-fields fields source))
+    (extract-fields fields source))
 
   Generator
   (to-generator [{:keys [gen]}] gen)
 
+  Application
+  (to-generator [{:keys [source operation]}]
+    (let [{:keys [op input output]} operation]
+      (map
+       #(to-tuple output (list (apply op (select-fields input %))))
+       source)))
+  
+  ;; this type is standard and could be part of the base logic
   TailStruct
   (to-generator [item]
     (:node item)))
@@ -46,6 +60,20 @@
   (generator [x]))
 
 (extend-protocol IGenerator
+  
+  ;; A bunch of generators that finally return  a seq
+  clojure.lang.IPersistentVector
+  (generator [v]
+    (generator (or (seq v) ())))
+  
+  clojure.lang.ISeq
+  (generator [v] v)
+
+  java.util.ArrayList
+  (generator [coll]
+    (generator (into [] coll)))
+
+  ;; These generators act differently than the ones above
   TailStruct
   (generator [sq]
     (compile-query sq))
@@ -57,11 +85,10 @@
 (defrecord ClojurePlatform []
   IPlatform
   (generator? [_ x]
-    ;; TODO: expand this to handle other types (like cascading.platform's)
-    true)
+    (satisfies? IGenerator x))
 
   (generator [_ gen output options]
-    (name-tuples output gen))
+    (to-tuples output (generator gen)))
 
   (to-generator [_ x]
     (to-generator x)))
