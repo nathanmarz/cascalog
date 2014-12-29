491d716012fe55bd5ad64fa52549e07249bb7778
bhiles <bennetthiles@gmail.com>
2014-12-29 10:34:17 -0500

    move test comment into actual test
    https://github.com/nathanmarz/cascalog/pull/256/files#r22298679

--- cascalog-core/src/clj/cascalog/cascading/flow.clj
+++ cascalog-core/src/clj/cascalog/cascading/flow.clj
@@ -105,15 +105,3 @@
   tuples."
   [m]
   (first (all-to-memory m)))
-
-;; TODO: this is cascading specific and should be moved
-(comment
-  (require '[cascalog.cascading.flow :as f])
-  "TODO: Convert to test."
-  (let [gen (-> (types/generator [1 2 3 4])
-                (ops/rename* "?x"))
-        pred (to-predicate * ["?a" "?a"] ["?b"])]
-    (fact
-     (f/to-memory
-      ((:op pred) gen ["?x" "?x"] "?z"))
-     => [[1 1] [2 4] [3 9] [4 16]])))
