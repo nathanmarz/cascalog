296ea47beec3d96b444fd332f96d2de6fc9f5642
Sam Ritchie <sritchie09@gmail.com>
2015-02-20 08:26:38 -0700

    add stats implementations

--- cascalog-core/src/clj/cascalog/cascading/stats.clj
+++ cascalog-core/src/clj/cascalog/cascading/stats.clj
@@ -1,6 +1,7 @@
 (ns cascalog.cascading.stats
   "Namespace implementing stats processing for the Cascading planner."
-  (:require [schema.core :as s])
+  (:require [clojure.string :refer [join]]
+            [schema.core :as s])
   (:import [cascading.stats CascadingStats]))
 
 ;; ## Schemas
@@ -10,9 +11,12 @@
 (def CounterName s/Str)
 
 (s/defschema CounterMap
+  "Clojure representation of Cascading's counters."
   {CounterGroup {CounterName s/Int}})
 
 (s/defschema StatsMap
+  "Clojure representation of the cascading.stats.CascadingStats data
+  structure."
   {:name (s/maybe s/Str)
    :counters CounterMap
    :duration s/Int
@@ -35,7 +39,10 @@
 (def ^:dynamic *flow-process* nil)
 (def ^:dynamic *op-call* nil)
 
-(def default-group "CascalogStats")
+(def default-group
+  "This is the default group name for any stats recorded in the course
+  of a Cascalog job."
+  "CascalogStats")
 
 ;; ## Stats Entry
 
@@ -57,10 +64,30 @@
   ([group :- CounterGroup counter :- CounterName]
      (inc-by! group counter 1)))
 
-;; ## Stats Output
+;; ## Stats Utilities
 
-(defn map-by [f xs]
+(defn map->json
+  "My own little JSON fn to prevent pulling in a JSON library."
+  [m]
+  (->> (for [[k v] m :let [v (cond (nil? v) "null"
+                                   (string? v) (format " \"%s\"" v)
+                                   (map? v) (map->json v)
+                                   :else v)]]
+         (format "\"%s\": %s" k v))
+       (join ", " )
+       (format "{%s}")))
+
+(defn map-by
+  "Takes a value-generating function and a sequence and returns a map
+  with the original seq elements as keys, and (f key) as each
+  value."
+  [f xs]
   (into {} (for [x xs] [x (f x)])))
+
+;; ## Cascading Stats Processing
+;;
+;; These functions turn the basic Cascading objects into Clojure data
+;; structures.
 
 (s/defn counter-map :- CounterMap
   "Digests the supplied stats object and returns a map of "
@@ -89,4 +116,36 @@
    :stopped? (.isStopped stats)
    :successful? (.isSuccessful stats)})
 
-;; ## Canned Stats Functions
+;; ## :stats-fn Implementations
+;;
+;; Drop these in to your Cascalog job as `(:stats-fn <f>)`, or wrap
+;; your job call in `(with-stats <f> ....)` to use them with the bare
+;; Cascading DSL.
+
+(s/defn stdout
+  ([] (stdout default-group))
+  ([group :- CounterGroup]
+     (s/fn [stats :- StatsMap]
+       (println (format "Custom counters for group %s:" group))
+       (doseq [[counter value] (get (:counters stats) group)]
+         (println (format "%s\t%s" counter value))))))
+
+(s/defn to-file
+  "path is passed to clojure.java.io/writer, so the argument can be a
+   Writer, BufferedWriter, OutputStream, File, URI, URL, Socket, and
+   String."
+  [path json? :- s/Bool]
+  (s/fn [stats :- StatsMap]
+    (spit path (if json? (map->json stats) (pr-str stats)))))
+
+(def clojure-file
+  "Returns a stats handler that prints the final stats map to the
+  supplied file (or output stream, etc, see clojure.java.io/writer) as
+  a Clojure data structure."
+  #(to-file % false))
+
+(def json-file
+  "Returns a stats handler that prints the final stats map to the
+  supplied file (or output stream, etc, see clojure.java.io/writer) as
+  a JSON data structure."
+  #(to-file % true))
