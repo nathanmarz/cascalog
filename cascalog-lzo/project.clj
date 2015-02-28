2e2cf2683f21ee18c8aec5a4ecf32ff96ddcd2d6
Philipp Winkler <philippwinkler@gmail.com>
2015-02-27 20:43:36 -0800

    Follow api changes from v1.0 -> v2.0. Also configure to work with Hadoop 2.4+

--- cascalog-lzo/project.clj
+++ cascalog-lzo/project.clj
@@ -6,14 +6,17 @@
   :description "Lzo compression taps for Cascalog."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"conjars.org" "http://conjars.org/repo"}
-  :dependencies [[com.twitter.elephantbird/elephant-bird-cascading2 "3.0.7"
+  :repositories {"conjars.org" "http://conjars.org/repo" "twttr.com" "http://maven.twttr.com/"}
+  :dependencies [[com.twitter.elephantbird/elephant-bird-cascading2 "4.6"
                   :exclusions [cascading/cascading-hadoop]]
-                 [hadoop-lzo "0.4.15"]]
+                 [com.hadoop.gplcompression/hadoop-lzo "0.4.19"]]
   :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :provided {:dependencies [[cascalog/cascalog-core ~VERSION]
-                                       [org.apache.hadoop/hadoop-core ~HADOOP-VERSION]
+                                       [org.apache.hadoop/hadoop-common "2.4.0"]
+                                       [org.apache.hadoop/hadoop-mapreduce-client-jobclient "2.4.0"]
                                        [org.apache.httpcomponents/httpclient "4.2.3"]]}
              :dev {:dependencies [[cascalog/midje-cascalog ~VERSION]]
                    :plugins [[lein-midje "3.1.3"]]}})
