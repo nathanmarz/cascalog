3302bf4db2ee35d116d639c59a90ac81b5673584
Sam Ritchie <sritchie09@gmail.com>
2015-03-05 11:29:46 -0700

    convert to multi module project

--- cascalog-lzo/project.clj
+++ cascalog-lzo/project.clj
@@ -1,22 +1,15 @@
 (def ROOT-DIR (subs *file* 0 (- (count *file*) (count "project.clj"))))
-(def HADOOP-VERSION (-> ROOT-DIR (str "/../HADOOP-VERSION") slurp))
 (def VERSION (-> ROOT-DIR (str "/../VERSION") slurp))
 
 (defproject cascalog/cascalog-lzo VERSION
   :description "Lzo compression taps for Cascalog."
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"conjars.org" "http://conjars.org/repo" "twttr.com" "http://maven.twttr.com/"}
+  :plugins [[lein-modules "0.3.11"]]
   :dependencies [[com.twitter.elephantbird/elephant-bird-cascading2 "4.6"
                   :exclusions [cascading/cascading-hadoop]]
                  [com.hadoop.gplcompression/hadoop-lzo "0.4.19"]]
-  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :provided {:dependencies [[cascalog/cascalog-core ~VERSION]
-                                       [org.apache.hadoop/hadoop-common "2.4.0"]
-                                       [org.apache.hadoop/hadoop-mapreduce-client-jobclient "2.4.0"]
-                                       [org.apache.httpcomponents/httpclient "4.2.3"]]}
-             :dev {:dependencies [[cascalog/midje-cascalog ~VERSION]]
-                   :plugins [[lein-midje "3.1.3"]]}})
+  :profiles {:provided {:dependencies
+                        [[cascalog/cascalog-core :version]
+                         [org.apache.hadoop/hadoop-common "2.4.0"]
+                         [org.apache.hadoop/hadoop-mapreduce-client-jobclient "2.4.0"]
+                         [org.apache.httpcomponents/httpclient "4.2.3"]]}
+             :dev {:dependencies [[cascalog/midje-cascalog :version]]}})
--- cascalog-more-taps/project.clj
+++ cascalog-more-taps/project.clj
@@ -1,21 +1,14 @@
 (def ROOT-DIR (subs *file* 0 (- (count *file*) (count "project.clj"))))
-(def HADOOP-VERSION (-> ROOT-DIR (str "/../HADOOP-VERSION") slurp))
 (def VERSION (-> ROOT-DIR (str "/../VERSION") slurp))
 
 (defproject cascalog/cascalog-more-taps VERSION
   :description "More taps for Cascalog"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :javac-options ["-target" "1.6" "-source" "1.6"]
+  :plugins [[lein-modules "0.3.11"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :jar-exclusions [#"\.java$"]
-  :repositories {"conjars.org" "http://conjars.org/repo"}
-  :profiles {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :provided {:dependencies [[cascalog/cascalog-core ~VERSION]
-                                       [org.apache.hadoop/hadoop-core ~HADOOP-VERSION]]}
-             :dev {:plugins [[lein-midje "3.1.3"]]
-                   :dependencies
-                   [[cascalog/midje-cascalog ~VERSION]
-                    [hadoop-util "0.3.0"]]}})
+  :profiles {:provided {:dependencies [[cascalog/cascalog-core :version]]}
+             :dev {:dependencies
+                   [[cascalog/midje-cascalog :version]
+                    [hadoop-util "_"]]}})
